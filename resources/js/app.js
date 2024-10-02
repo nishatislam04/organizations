501a725ab07346b1ffd464bd8669f48d8c827b83
nio04 <nishatislam3108@gmail.com>
2024-10-02 13:56:11 +0600

    add: jquery env setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,6 @@
 import './bootstrap';
 import 'flowbite';
+import $ from "jquery";
+import "./org/checkboxes.js"
+
+window.$ = $;
--- resources/js/index.js
+++ resources/js/index.js
@@ -2,5 +2,5 @@ import 'flowbite/dist/flowbite.js';
 import './sidebar';
 import './charts';
 import './dark-mode';
-
+import "./app"
 // Have the courage to follow your heart and intuition.
