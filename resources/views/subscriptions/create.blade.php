8fa2f04a025529eda668a9c72a8466d1072378c1
nio04 <nishatislam3108@gmail.com>
2024-10-08 10:40:56 +0600

    fix: close btn - create subs redirection change

--- resources/views/subscriptions/create.blade.php
+++ resources/views/subscriptions/create.blade.php
@@ -14,7 +14,8 @@
 
             <x-buttons.button
               class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white"
-              type="a" href='{{ route("organizations.index") }}'>
+              type="a"
+              href='{{ route("organizations.show", $organization->id) }}'>
               <img class="w-5 h-5"
                 src="{{ Vite::asset("resources/icons/close-icon.svg") }}"
                 alt="">
