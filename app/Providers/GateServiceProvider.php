97c6004292b5d808336c6cca629fe09a3f6edc93
nio04 <nishatislam3108@gmail.com>
2024-10-09 15:21:41 +0600

    update: admin user can not edit, update org

--- app/Providers/GateServiceProvider.php
+++ app/Providers/GateServiceProvider.php
@@ -33,11 +33,13 @@ class GateServiceProvider extends ServiceProvider {
         });
 
         Gate::define("org-delete", function (User $user, Organization $organization) {
-            return $user->id === $organization->user_id || $user->role === "super";
+            // return $user->id === $organization->user_id || $user->role === "super";
+            return $user->role === "super";
         });
 
         Gate::define("org-edit", function (User $user, Organization $organization) {
-            return $user->id === $organization->user_id || $user->role === "super";
+            // return $user->id === $organization->user_id || $user->role === "super";
+            return $user->role === "super";
         });
 
         Gate::define("user-edit", function (User $user) {
